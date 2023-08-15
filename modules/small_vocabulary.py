07712e1f34963459f53d3738f41549b86865ef9f
Bris <Bris@nowhere.org>
2023-08-16 01:52:51 +0200

    remove copied code

--- modules/small_vocabulary.py
+++ modules/small_vocabulary.py
@@ -1,9 +1,4 @@
 from lib.discordspeak import Module
 
 class SmallVocabulary(Module):
-    def on_key(self, key):
-        if helpers.chance(1/100):
-            if key.name not in keyboard_near or helpers.chance(1/2):
-                return []
-            else:
-                return [random.choice(keyboard_near[key.name])]
+    pass
